fae5f7d0515ea99ca22cc1295e0f0c1cefb560a7
Erik Nelson <erik@nsk.io>
2019-03-29 14:59:20 -0400

    Update with first tests

---
+++ src/client/resources/resources.test.ts
@@ -0,0 +1,19 @@
+import { MigResource } from './mig';
+
+test('MigResource namedPath should be correct', () => {
+  const ns = 'test-ns';
+  const planName = 'eriks-plan';
+  const migResource = new MigResource(MigResource.Kind.MigPlan, ns);
+  expect(migResource.namedPath(planName)).toBe(
+    `/apis/migration.openshift.io/v1alpha1/namespaces/${ns}/migplans/${planName}`
+  )
+})
+
+test('MigResource listPath should be correct', () => {
+  const ns = 'test-ns';
+  const planName = 'eriks-plan';
+  const migResource = new MigResource(MigResource.Kind.MigPlan, ns);
+  expect(migResource.listPath()).toBe(
+    `/apis/migration.openshift.io/v1alpha1/namespaces/${ns}/migplans`
+  )
+})

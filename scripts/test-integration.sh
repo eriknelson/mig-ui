01ff532d8cb5f51f3ac61ec28be4cbba6a3a8c07
Erik Nelson <erik@nsk.io>
2019-05-30 11:19:46 -0400

    Updated test-integration script

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cp ./config/config.dev.json.example ./config/config.dev.json
-./node_modules/.bin/start-server-and-test start http://localhost:9000 './node_modules/.bin/cypress run'
+_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+projectRoot="$_dir/.." # Refers to the root of the git project
+exDevConfigFile="$projectRoot/config/config.dev.json.example"
+devConfigFile="$projectRoot/config/config.dev.json"
+
+if [[ ! -f "$devConfigFile" ]]; then
+  cp "$exDevConfigFile" "$devConfigFile"
+fi
+
+# TODO: This server dest+port should be parameterized somehow
+$projectRoot/node_modules/.bin/start-server-and-test start http://localhost:9000 "$projectRoot/node_modules/.bin/cypress run"
